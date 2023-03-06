ecd1d7c48b9d5753e5087c22f91fd62176fc0f80
Mauricio de Mendonça Neto <mauriciont191@gmail.com>
2023-03-06 14:13:19 +0000

    Att, comando case

--- Script_useradd.sh
+++ Script_useradd.sh
@@ -5,36 +5,45 @@
 
 #-Como utilizar o script: Esse Script é necessário digitar a frente a #quantidade de user a ser criado apenas ex: "./Script 5" estarei infor#mando que 5 user devem ser criados.
 
-read -p "Deseja criar diretórios? " resp
+read -p "Deseja criar diretórios?[S/n] " resp
 
 x=1
 
-if [ $resp != "n" ]
-then
-  read -p "Quantos diretórios deseja criar? " qd
-  while [ $x -le $qd ]
-  do
-    read -p "Qual o nome do diretório? " nomed 
-    mkdir $nomed
-  done
-fi
+case $resp in
+  "S" | "s")
+    read -p "Quantos diretórios deseja criar? " qd
+    while [ $x -le $qd ]
+    do
+      read -p "Qual o nome do diretório? " nomed 
+      mkdir $nomed
+      x=$[ x + 1 ]
+    done;;
+    "N" | "n")
+      echo "Sem crição de diretório";;
+    *)
+      echo "Opção invalida";;
+esac
+
 x=1
+
 read -p "Criará grupo?[S/n] " resp
 
-if [ $resp != "n" ] 
-then
-  read -p "Quantos? " qg
+case $resp in
+  "S" | "s")
+    read -p "Quantos? " qg
 
-  while [ $x -le $qg ]
-  do
-    read -p "Qual o nome do $x? " gp
-    groupadd $gp
-    x=$[ $x + 1 ]
-  done
-
-else
-  echo "Sem criação de grupo."
-fi
+    while [ $x -le $qg ]
+    do
+      read -p "Qual o nome do $x? " gp
+      groupadd $gp
+      x=$[ $x + 1 ]
+    done;;
+  
+  "N" | "n")
+    echo "Sem criação de grupo";;
+  *)
+    echo "Operação invalida";;
+esac
 
 x=0
 
@@ -42,22 +51,24 @@ read -p "Quantos user serão adcicionado? " qu
 
 while [ $x -lt $qu ]
 do
-x=$[ $x + 1 ]
-read -p "Qual o nome do $x"o" user? " nome
-read -p "Vai adicionar os user a algum grupo?[S/n] " resp
-
-if [ $resp != "n" ]
-then
-  read -p "Qual o grupo ele será adcionado? " group
-  useradd $nome -c "convidado$x" -m -s /bin/bash -p $(openssl passwd senha) -g $group
-  passwd $nome -e
-  echo "-> foi adicionado convidado$x"
-
-else
-  useradd $nome -c "convidado$x" -m -s /bin/bash -p $(openssl passwd senha)
-fi
+  x=$[ $x + 1 ]
+  read -p "Qual o nome do $x"o" user? " nome
+  tail -n5 /etc/group
+  read -p "Vai adicionar os user a algum grupo?[S/n] " resp
+  
+  case $resp in
+    "S" | "s")
+      read -p "Qual o grupo ele será adcionado? " group
+      useradd $nome -c "convidado$x" -m -s /bin/bash -p $(openssl passwd senha) -G $group
+      passwd $nome -e
+      echo "-> foi adicionado $nome ao grupo " 
+      cat /etc/group | grep $group;;
+    "N" | "n")
+      useradd $nome -c "convidado$x" -m -s /bin/bash -p $(openssl passwd senha);;
+    *)
+      echo "Opção invalida!";;
+  esac
 done
-
 
 #A senha inicial é "senha"
 
